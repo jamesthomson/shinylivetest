131885dbfcb23a0460c1c3d5c617cde3303aaff9
James Thomson (Annalect UK) <james.thomson@omc.com>
2025-11-12 11:00:37 +0000

    minor changes

--- processing script.R
+++ processing script.R
@@ -1,14 +1,15 @@
 
 location <- 'C:/Users/JamesThomson(Annalec/OneDrive - OneWorkplace/Documents 1/Git Repos/shiny_testing'
 
-
+# test app on shiny
 shiny::runApp(paste0(location, '/app'))
 
-
+# export app to github pages using shinylive
 shinylive::export(paste0(location, '/app'),
-                  paste0(location, '/docs') #must be docs to work with githubpages
-                   )
+                  paste0(location, '/docs')) # must be docs to work with github pages
+                   
 
+# test app on local server
 httpuv::runStaticServer(paste0(location, '/docs'))
 
                         
